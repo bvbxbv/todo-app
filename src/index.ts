eb2d1319131b8502e0f79c3f2af4d9407d687991
bvbxbv <aburach@inbox.ru>
2025-12-10 01:39:00 +0700

    refactor(layout): split main.scss into files
    - created directory components for all ui components (header, calendar and so on)
    - created directory ui for default html components style overriding
    - todo items now generating in index.ts. Removed duplicated html

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
 import "./scss/main.scss";
+
+const todoList = document.getElementById("todo-list");
+
+if (todoList !== null) {
+  for (let i = 0; i < 24; i++) {
+    todoList.innerHTML += `
+		<div class="todo-item">
+										<div class="group">
+											<div class="__content">
+												<div class="group">
+													<div class="__title">
+														<span
+															>> Lorem ipsum dolor sit amet,
+															consectetur adipisicing elit. Doloremque
+															sit tenetur voluptatum ratione ab qui
+															soluta fugiat vitae cumque officia
+															maiores iste enim dolorum, odio sed
+															ullam est possimus iure!</span
+														>
+													</div>
+													<div class="__controls">
+														<button>
+															<i class="ti ti-edit"></i>
+														</button>
+
+														<button>
+															<i class="ti ti-check"></i>
+														</button>
+
+														<button>
+															<i class="ti ti-trash"></i>
+														</button>
+													</div>
+												</div>
+												<div class="__description">
+													Lorem, ipsum dolor sit amet consectetur
+													adipisicing elit. Velit earum iste ad fugiat
+													dicta, libero iure perspiciatis facilis amet
+													minus consequatur perferendis autem, ullam
+													praesentium. Ratione aut consectetur autem a.
+												</div>
+											</div>
+										</div>
+
+										<div class="__timestamp">
+											========================[22.02.2004]========================
+										</div>
+									</div>`;
+  }
+}
